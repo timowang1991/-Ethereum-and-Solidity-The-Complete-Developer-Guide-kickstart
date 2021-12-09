4605740ebb33f4a75c66f8b85e9d705712ccd768
Timo Wang <timo@steaker.com>
2021-12-09 15:01:15 +0800

    UT complete

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -49,4 +49,64 @@ describe('Campaigns', () => {
         const isContributor = await campaign.methods.approvers(accounts[1]).call()
         assert(isContributor);
     });
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                from: accounts[1],
+                value: '99'
+            });
+            assert(false);
+        } catch (error) {
+            assert(error);
+        }
+    });
+
+    it('allows a manager to make a payment request', async () => {
+        await campaign.methods.createRequest(
+            'buy batteries',
+            '99999',
+            accounts[1]
+        ).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        const request = await campaign.methods.requests(0).call();
+        
+        assert.equal(request.description, 'buy batteries');
+    });
+
+    it('processes requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether')
+        });
+
+        await campaign.methods.createRequest(
+            'buy batteries',
+            web3.utils.toWei('5', 'ether'),
+            accounts[1]
+        ).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        let balance = await web3.eth.getBalance(accounts[1]);
+        balance = web3.utils.fromWei(balance, 'ether');
+        balance = parseFloat(balance);
+
+        console.log('balance', balance);
+        assert(balance > 104);
+    });
 });
