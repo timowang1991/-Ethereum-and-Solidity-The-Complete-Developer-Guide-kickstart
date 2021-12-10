f51895e54d4b907e423ca3291917e02c7562fb7e
Timo Wang <timo@steaker.com>
2021-12-10 15:36:16 +0800

    onFinalize ready

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
+import Campaign from '../ethereum/campaign';
 
 class RequestRow extends Component {
+    onApprove = async () => {
+        const { id, address } = this.props;
+        const campaign = Campaign(address);
+        const accounts = await web3.eth.getAccounts();
+
+        await campaign.methods.approveRequest(id).send({
+            from: accounts[0]
+        });
+    }
+
+    onFinalize = async () => {
+        const { id, address } = this.props;
+        const campaign = Campaign(address);
+        const accounts = await web3.eth.getAccounts();
+
+        await campaign.methods.finalizeRequest(id).send({
+            from: accounts[0]
+        });
+    }
+    
     render() {
         const { Row, Cell } = Table;
         const {
@@ -20,6 +41,12 @@ class RequestRow extends Component {
                 <Cell>{web3.utils.fromWei(value, 'ether')}</Cell>
                 <Cell>{recipient}</Cell>
                 <Cell>{approvalCount}/{approversCount}</Cell>
+                <Cell>
+                    <Button color="green" basic onClick={this.onApprove}>Approve</Button>
+                </Cell>
+                <Cell>
+                    <Button color="blue" basic onClick={this.onFinalize}>Finalize</Button>
+                </Cell>
             </Row>
         );
     }
